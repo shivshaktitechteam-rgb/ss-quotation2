c66f468d6dfaffc9ba9c425a6a340dbcd1cface0
shivshaktitechteam-rgb <shivshakti.techteam@gmail.com>
2026-03-10 04:33:53 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 // api.js
 // Paste your Apps Script Web App URL here (ends with /exec)
-const API_BASE = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const API_BASE = "https://script.google.com/macros/s/AKfycbz7MZBswelTi1Wm5jeOVDnUbe3DzE5LcqBJ0qO478s3dwErNSWF2gGoYjcad9ttbmsupQ/exec";
 
 async function apiGet(params) {
   const url = API_BASE + "?" + new URLSearchParams(params).toString();
